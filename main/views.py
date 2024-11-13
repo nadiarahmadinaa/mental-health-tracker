d5be06cde98fc9e4eacfadacf44722084fa985aa
nadiarahmadinaa <nadiarahmadinaaristawati@gmail.com>
2024-11-13 14:25:01 +0700

    Tutorial 8

--- main/views.py
+++ main/views.py
@@ -13,7 +13,28 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
 import datetime
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
 from django.urls import reverse
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
 
 def create_mood_entry(request):
     form = MoodEntryForm(request.POST or None)
